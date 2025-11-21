2a8dd5b61a99e971ea926647dfe4a7162a69c8e8
mfumattew <fmabouba@byupathway.edu>
2025-11-21 07:32:59 +0100

    Fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fractions operation = new Fractions();
+
+        Console.WriteLine("First Fraction");
+        operation.Fraction();
+        Console.WriteLine(operation.GetFractionString());
+        Console.WriteLine(operation.GetDecimalValue());
+
+        Console.WriteLine("Second Fraction");
+        operation.Fraction(5);
+        Console.WriteLine(operation.GetFractionString());
+        Console.WriteLine(operation.GetDecimalValue());
+
+        Console.WriteLine("Third Fraction");
+        operation.Fraction(3,4);
+        Console.WriteLine(operation.GetFractionString());
+        Console.WriteLine(operation.GetDecimalValue());
+
+        Console.WriteLine("Fourth Fraction");
+        operation.Fraction(1,3);
+        Console.WriteLine(operation.GetFractionString());
+        Console.WriteLine(operation.GetDecimalValue());
     }
 }
